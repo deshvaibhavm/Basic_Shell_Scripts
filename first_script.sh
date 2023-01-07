fe2ee43389d5dda231be94d7a6afe991414515a7
deshvaibhavm <100898952+deshvaibhavm@users.noreply.github.com>
2023-01-07 19:43:15 +0530

    Update first_script.sh

--- first_script.sh
+++ first_script.sh
@@ -5,6 +5,8 @@ echo "Lets move on to script dir and print the same"
 cd /Users/Shared/Practice/Scripts/
 echo `pwd`
 
+touch test_test.text
+
 echo "Execute command to direct the output to a file"
 ls > /Users/Shared/Practice/output/list_of_files
 
