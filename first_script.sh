65367d258a949ef9af287e10e8f803cc43289711
deshvaibhavm <100898952+deshvaibhavm@users.noreply.github.com>
2023-01-07 19:38:00 +0530

    Create first_script.sh

---
+++ first_script.sh
@@ -0,0 +1,14 @@
+#!/bin/bash -x
+
+echo "Lets move on to script dir and print the same"
+
+cd /Users/Shared/Practice/Scripts/
+echo `pwd`
+
+echo "Execute command to direct the output to a file"
+ls > /Users/Shared/Practice/output/list_of_files
+
+echo "Below is the inside file content of list_of_files"
+cat /Users/Shared/Practice/output/list_of_files
+
+exit 0
