07a9b534edfdf61d2cbc080e6228165038eaf8f2
deshvaibhavm <100898952+deshvaibhavm@users.noreply.github.com>
2023-01-14 11:33:21 +0530

    Create simple_conditional_if.sh

---
+++ simple_conditional_if.sh
@@ -0,0 +1,18 @@
+#!/bin/bash -x
+
+export ymd=`date "+%Y%m%d"`
+
+echo "Today's date is $ymd"
+
+export path1="/Users/Shared/Practice/dir1/"
+
+export file=test
+export extn=".txt"
+
+if ls $path1$file$extn
+then
+{
+    echo "$path1$file$extn File exist"
+    exit 0
+}
+fi
